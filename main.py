275df796de09e4dcaa459b9e562bcf16770081ca
iSiut <illiasiutkin07@gmail.com>
2022-07-23 23:08:06 -0500

    add rng and add help descriptions

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import discord
 from discord.ext import commands
 import json
+import random
 
-bot = commands.Bot(command_prefix="*")
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("*")) # prefix for calling the bot (or when mentioned)
 
 f = open('config.json', "r")
-token = json.loads(f.read())
+token = json.loads(f.read()) # config.json has the token as a string
 
 
 @bot.event
 async def on_ready():
-    print("Botto is ready!")
+    print(f"{bot.user} is ready!") #executed when bot is deployed and ready
 
-@bot.command()
+
+@bot.command(description="returns the latency", brief="*ping")
 async def ping(ctx):
     await ctx.send(f"✅ The ping is: {round(bot.latency, 4)} seconds.")
+
+
+@bot.command(description="returns a random number between two args if given", brief="*rng [from this] [to this]")
+async def rng(ctx, first='1', last='100'):
+    if first >= last:
+        await ctx.send("The second value has to be greater than the first value.")
+    else:
+        await ctx.send(random.randint(int(first), int(last)))
+
+
+
 
 
 bot.run(token)
