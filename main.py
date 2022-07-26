7d5a77aa0e45725008be106de7012dfd91115dcf
iSiut <illiasiutkin07@gmail.com>
2022-07-25 22:18:22 -0500

    add avatar command (error checking needed)

--- main.py
+++ main.py
@@ -28,13 +28,14 @@ async def rng(ctx, first='1', last='100'):
         print(f"{ctx.author} executed rng (args error)")
     else:
         await ctx.send(random.randint(int(first), int(last)))
-        print(f"{ctx.author} executed rng")
+        print(f"{ctx.author} executed rng (success)")
 
 
 @bot.command(description="calculates gpa with A and B grades as inputs", brief="*gpa [a grades] [b grades]")
 async def gpa(ctx, *args):
     if len(args) != 2:
         await ctx.send("Please enter A and B amounts.\nExample: *gpa 35 3")
+        print(f"{ctx.author} executed gpa (args length error)")
         return
 
     try:
@@ -43,8 +44,23 @@ async def gpa(ctx, *args):
 
         gpa = ((a*4)+(b*3))/(a+b)
         await ctx.send(f"Your GPA is {gpa}")
+        print(f"{ctx.author} executed gpa (success)")
     except:
         await ctx.send("Please enter valid integer A and B amounts.\nExample: *gpa 35 3")
+        print(f"{ctx.author} executed gpa (invalid args)")
+
+
+@bot.command()
+async def avi(ctx, member: discord.Member = None):
+    if member is not discord.Member:
+        await ctx.send(ctx.author.avatar_url)
+    else:
+        av = member.avatar_url
+        await ctx.send(av)
+
+    
+
+
 
 
 bot.run(token)
